f555c8f1365c9c27dff663dc535de2e28ddbac11
Abdullah <altaiabod43@gmail.com>
2026-07-10 18:24:16 +0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE="ibraq-store-v1";
-const FILES=["./","./index.html","./style.css","./store.js","./app.js","./ibraq-logo.jpg","./apple-touch-icon.png","./icon-192.png","./icon-512.png","./manifest.json"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
